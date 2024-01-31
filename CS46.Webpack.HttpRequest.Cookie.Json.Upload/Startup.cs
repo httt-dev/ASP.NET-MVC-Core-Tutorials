6f0550ec0a85eeb1eb0e18d5c98ca324c38d3afb
hoa.nguyen <httt.dev@gmail.com>
2024-01-31 21:51:50 +0700

    asp-net-core-doc-va-ghi-request-va-response-xu-ly-truy-van-co-ban-upload-file-cookie-json

--- CS46.Webpack.HttpRequest.Cookie.Json.Upload/Startup.cs
+++ CS46.Webpack.HttpRequest.Cookie.Json.Upload/Startup.cs
@@ -2,8 +2,10 @@ using CS46.Webpack.Request.Cookie.Json.Upload.mylib;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -75,18 +77,106 @@ namespace CS46.Webpack.Request.Cookie.Json.Upload
                     await context.Response.WriteAsync("Encoding");
                 });
 
-                endpoints.MapGet("/Cookies", async (context) =>
+                // path : cookies/write
+                //        cookies/read
+
+                // *action : * co nghia la khong co chuoi chuoi action cung duoc
+                // vi du : /Cookies/
+                //          /Cookies/abc  => action = abc
+                //          /Cookies/zyx 
+                endpoints.MapGet("/Cookies/{*action}", async (context) =>
                 {
-                    await context.Response.WriteAsync("Cookies");
+                    // la data ma server gui ve cho client
+                    // khi trinh duyet co cookies thi se luu lai va dung cho cac lam truy van sau 
+                    // khi da co cookies thi khi truy van client se gui kem thong tin cookies len server
+
+                    var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), context.Request);
+
+                    // lay gia tri action tu route
+                    var action = context.GetRouteValue("action")  ?? "read";
+
+                    string message = "";
+                    if (action.ToString() == "write")
+                    {
+                        // ten = gia tri cua cookie 
+                        // thoi gian hieu luc
+
+                        
+
+                        var option = new CookieOptions
+                        {
+                            Path = "/",
+                            Expires = DateTime.Now.AddDays(1)
+                        };
+
+                        context.Response.Cookies.Append("masanpham", "SP-ABC" , option);
+                        message = "Cookies duoc ghi";
+
+                    }
+                    else
+                    {
+                        // lay danh sach cac cookies
+
+                        var listCookies = context.Request.Cookies.Select((cookie) => $"{cookie.Key} : {cookie.Value}".HtmlTag("li"));
+
+                        message = string.Join("", listCookies).HtmlTag("ul");
+                    }
+
+                    var huongdan = "<a href=\"/Cookies/read\"> Doc cookie</a><br><a href=\"/Cookies/write\"> Ghi cookie</a>";
+
+                    huongdan = huongdan.HtmlTag("div", "container mt-4");
+                    message = message.HtmlTag("div", "alert-danger");
+
+                    var html = HtmlHelper.HtmlDocument("Thong tin Cookies : " + action, menu + huongdan + message);
+                    //context.Request
+                    var info = RequestProcess.RequestInfo(context.Request).HtmlTag("div", "container");
+
+                    await context.Response.WriteAsync(html);
+
                 });
                 endpoints.MapGet("/Json", async (context) =>
                 {
-                    await context.Response.WriteAsync("Json");
+                   
+                    var p = new
+                    {
+                        TenSP = "Dong ho garmin",
+                        Gia = 100000,
+                        NgaySX = new DateTime(2023,1,12)
+                    };
+
+                    context.Response.ContentType = "application/json";
+
+                    var json = JsonConvert.SerializeObject(p);
+
+                    await context.Response.WriteAsync(json);
                 });
-                endpoints.MapGet("/Form", async (context) =>
+                endpoints.MapMethods("/Form", new string[] { "POST" , "GET"}, async (context) =>
                 {
-                    await context.Response.WriteAsync("Form");
+                    var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), context.Request);
+
+                    var formHtml = await RequestProcess.ProcessForm(context.Request);
+
+                    var html = HtmlHelper.HtmlDocument("Form", menu+ formHtml);
+
+                    //context.Request
+                    var info = RequestProcess.RequestInfo(context.Request).HtmlTag("div", "container");
+
+                    await context.Response.WriteAsync(html);
                 });
+
+                //endpoints.MapPost("/Form", async (context) =>
+                //{
+                //    var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), context.Request);
+
+                //    var formHtml = RequestProcess.ProcessForm(context.Request);
+
+                //    var html = HtmlHelper.HtmlDocument("Form", menu + formHtml);
+
+                //    //context.Request
+                //    var info = RequestProcess.RequestInfo(context.Request).HtmlTag("div", "container");
+
+                //    await context.Response.WriteAsync(html);
+                //});
 
             });
         }
--- CS46.Webpack.HttpRequest.Cookie.Json.Upload/mylib/RequestProcess.cs
+++ CS46.Webpack.HttpRequest.Cookie.Json.Upload/mylib/RequestProcess.cs
@@ -2,13 +2,96 @@
 
 using Microsoft.AspNetCore.Http;
 using System;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CS46.Webpack.Request.Cookie.Json.Upload.mylib
 {
     public static class RequestProcess
     {
+        // Xử lý khi HTML Form post dữ liệu
+        public static async Task<string> ProcessForm(HttpRequest request)
+        {
+
+            //Xử lý đọc dữ liệu Form - khi post - dữ liệu này trình  bày trên Form
+            string hovaten = "";
+            bool luachon = false;
+            string email = "";
+            string password = "";
+            string thongbao = "";
+
+            // Đọc dữ liệu từ Form do truy vấn gửi đến (chỉ xử lý khi là post)
+            if (request.Method == "POST")
+            {
+                IFormCollection _form = request.Form;
+
+                email = _form["email"].FirstOrDefault() ?? "";
+                hovaten = _form["hovaten"].FirstOrDefault() ?? "";
+                password = _form["password"].FirstOrDefault() ?? "";
+                luachon = (_form["luachon"].FirstOrDefault() == "on");
+
+                thongbao = $@"Dữ liệu post - email: {email}
+                          - hovaten: {hovaten} - password: {password}
+                          - luachon: {luachon} ";
+
+
+                // var filePath = Path.GetTempFileName();
+                // Xử lý nếu có file upload (hình ảnh,  ... )
+                //if (_form.Files.Count > 0)
+                //{
+                //    string thongbaofile = "Các file đã upload: ";
+                //    foreach (IFormFile formFile in _form.Files)
+                //    {
+                //        if (formFile.Length > 0)
+                //        {
+                //            var filePath = "wwwroot/upload/" + formFile.FileName;    // Lấy tên  file
+                //            if (!Directory.Exists("wwwroot/upload/")) Directory.CreateDirectory("wwwroot/upload/");
+                //            thongbaofile += $"{filePath} {formFile.Length} bytes";
+                //            using (var stream = new FileStream(filePath, FileMode.Create)) // Mở stream để lưu file, lưu file ở thư mục wwwroot/upload/
+                //            {
+                //                await formFile.CopyToAsync(stream);
+                //            }
+                //        }
+
+                //    }
+                //    thongbao += "<br>" + thongbaofile;
+                //}
+                
+                // xu lu upload file 
+
+                if ( _form.Files.Count > 0)
+                {
+                    string thongbaofile = "Các file đã upload:";
+                    foreach (IFormFile formFile in _form.Files)
+                    {
+                        if (formFile.Length > 0)
+                        {
+                            var filePath = "wwwroot/upload/" + formFile.FileName;
+                            if (!Directory.Exists("wwwroot/upload/"))
+                            {
+                                Directory.CreateDirectory("wwwroot/upload/");
+                            }
+                            thongbaofile += $"{filePath} {formFile.Length} bytes";
+                            using(var stream = new FileStream(filePath , FileMode.Create))
+                            {
+                                await formFile.CopyToAsync(stream);
+                            }
+                        }
+                    }
+                    thongbao += "<br>" + thongbaofile;
+                }
+            }
+
+            var format =  await File.ReadAllTextAsync("formtest.html");
+
+            //var html = string.Format(format, "hoten", "mail@gmail.com", "no");
+            var html = string.Format(format, hovaten, email, luachon?"checked" : "") + thongbao;
+
+            return html.HtmlTag("div","container");
+        }
+
         public static string RequestInfo(HttpRequest request)
         {
             if (request is null)
